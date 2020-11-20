73e5b247391c38b9b5d56040bde9516be4d72b81
Techmokid <58498119+Techmokid@users.noreply.github.com>
2020-11-20 21:55:53 +0800

    Updated Betting Algorithm

--- Active Projects/Betting Algorithm.py
+++ Active Projects/Betting Algorithm.py
@@ -1,17 +1,22 @@
-import random
+import random,math
 
+# sin(45) is the "Break even" point
 probabilityOfWinning = 0.5
 startingBet = 1
-availableMoney = 10_000
-betMultiplier = 3
+availableMoney = 100_000_000
+betMultiplier = 2
 winningsMultiplier = 2
 
 currentBet = startingBet
 maximumAmount = 0
+looper = 0
 while(availableMoney > 0):
+    looper += 1
     if (maximumAmount < availableMoney):
         maximumAmount = availableMoney
-    
+    if (looper == 1_000_000):
+        print(maximumAmount)
+        looper = 0
     randomPick = random.random()
     
     if (randomPick <= probabilityOfWinning):
