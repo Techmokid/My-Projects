1329930833208281fdc17c5fa80c08579ed2e668
Techmokid <aj2000wa@gmail.com>
2021-11-24 17:50:45 +0800

    Added VR Suit Code

---
+++ Active Projects/VR Suit/Motor_Controllers/Control_API.ino
@@ -0,0 +1,36 @@
+void setMotorSpeed(int motorID, int in) { motorID *= 2; setMotorSpeed(motorID+2,motorID+3,in); }
+void setMotorSpeed(int pinA, int pinB, int in) {
+  if (in == 0) {
+    analogWrite(pinA,0);
+    analogWrite(pinB,0);
+  } else if (in == 1) {
+    analogWrite(pinB,0);
+    analogWrite(pinA,255);
+  } else if (in == -1) {
+    analogWrite(pinA,0);
+    analogWrite(pinB,255);
+  } else if (in > 0) {
+    analogWrite(pinA,0);
+    analogWrite(pinB,clamp(in,0,255));
+  } else {
+    analogWrite(pinB,0);
+    analogWrite(pinA,clamp(in,0,255));
+  }
+}
+
+void setupMotors() {
+  for (int i = 2; i < 55; i++) {
+    pinMode(i,OUTPUT);
+    analogWrite(i,0);
+  }
+}
+
+void emergencyShutoff() {
+  for (int i = 2; i < 55; i++) {
+    analogWrite(i,0);
+  }
+}
+
+int clamp(int in,int max,int min) {
+  return min(max(in,min),max);
+}
