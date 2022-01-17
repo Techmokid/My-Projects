0d9fd7d3231157a3c856e5816a7181eaf955fadd
Techmokid <58498119+Techmokid@users.noreply.github.com>
2022-01-17 16:32:25 +0800

    Corrected direction of 4x4x4 matrix

--- Active Projects/4x4x4_LED_Cube/4x4x4_LED_Cube.ino
+++ Active Projects/4x4x4_LED_Cube/4x4x4_LED_Cube.ino
@@ -9,9 +9,9 @@ void setup() { setupLEDs(); }
 void loop() {
   int posX = random(0,4);
   int posY = random(0,4);
-  setPixel(posX,posY,3,HIGH);
-  carefulDelay(250);
-  moveDisplayDown();
+  setPixel(posX,3,posY,HIGH);
+  carefulDelay(500);
+  //moveDisplayDown();
 }
 
 void carefulDelay(unsigned long timer) {
@@ -23,11 +23,11 @@ void carefulDelay(unsigned long timer) {
 
 void moveDisplayDown() {
   for (int x = 0; x < 4; x++) {
-    for (int y = 0; y < 4; y++) {
-      LED_MEM[x][y][0] = LED_MEM[x][y][1];
-      LED_MEM[x][y][1] = LED_MEM[x][y][2];
-      LED_MEM[x][y][2] = LED_MEM[x][y][3];
-      LED_MEM[x][y][3] = 0;
+    for (int z = 0; z < 4; z++) {
+      LED_MEM[x][0][z] = LED_MEM[x][1][z];
+      LED_MEM[x][1][z] = LED_MEM[x][2][z];
+      LED_MEM[x][2][z] = LED_MEM[x][3][z];
+      LED_MEM[x][3][z] = 0;
     }
   }
 }
