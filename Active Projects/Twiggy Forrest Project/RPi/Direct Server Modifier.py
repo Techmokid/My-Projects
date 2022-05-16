39f4c99f56b2044bebc94628329a776972592e08
Techmokid <58498119+Techmokid@users.noreply.github.com>
2022-05-17 01:51:08 +0800

    Added GPS to twiggy forrest project

--- Active Projects/Twiggy Forrest Project/RPi/Direct Server Modifier.py
+++ Active Projects/Twiggy Forrest Project/RPi/Direct Server Modifier.py
@@ -1,13 +1,22 @@
-import requests
+import requests,hashlib,hmac
 
-URL = "http://192.168.1.15:8080/index2.php"
+URL = "http://192.168.1.15:666/index2.php"
 
-def setServerValue(key, val):
-    global ID
-    
-    serverResponse = requests.get(url=URL,headers={'Setvalue':val,'Key':key,'Id':ID})
+def encryptData(data,key):
+    return hmac.new(bytes(key , 'utf-8'), msg = bytes(data , 'utf-8'), digestmod = hashlib.sha256).hexdigest()
+
+def stripContent(x):
+    content = str(x.content)
+    startVal = content.find("<body>") + len("<body>")
+    endVal = content.rfind("</body>")
+    return content[startVal+2:endVal]
+
+def setServerValue(ID,key, val,encryptedKey):
+    serverResponse = requests.get(url=URL,headers={'Setvalue':val,'Key':key,'Id':ID,'Encryptedkey':encryptData(val+key+ID,encryptedKey)})
     if (serverResponse.ok == False):
         raise Exception("Error when getting server response LOL")
+    print(stripContent(serverResponse))
 
-ID = input("ID: ")
-setServerValue(input("Key: "),input("Value: "))
+print(stripContent(requests.get(url=URL,headers={'getinfoonid':input("ID: ")})))
+#print(stripContent(requests.get(url=URL,headers={'Newid':'1'})))
+#setServerValue(input("ID: "),input("Key: "),input("Value: "),input("Encryption Key: "))
--- Active Projects/Twiggy Forrest Project/RPi/RPi Controller Code.py
+++ Active Projects/Twiggy Forrest Project/RPi/RPi Controller Code.py
@@ -1,15 +1,14 @@
 from multiprocessing import Process,Manager,Value
-import requests,json,os,time
+import requests,json,os,time,hashlib,hmac,serial,sys
 import RPi.GPIO as GPIO
 
 # https://www.w3schools.com/python/ref_requests_response.asp
 
 #URL = "http://www.techmo.unity.chickenkiller.com"
-URL = "http://192.168.1.15:8080/index2.php"
+URL = "http://192.168.1.15:666/index2.php"
 filepath = "/home/pi/Desktop/"
 ID_Filepath = filepath + "ID.txt"
-Status_Down_Filepath = filepath + "Status.down"
-Status_Up_Filepath = filepath + "Status.up"
+EncryptionKey_Filepath = filepath + "key.txt"
 usingCurrentSensor = False
 
 warningPin = 13
@@ -33,17 +32,56 @@ sharedData.append("-1")         # ID
 sharedData.append(None)         # Temp variable for server response
 sharedData.append([])           # Amp reading average readings list
 sharedData.append(False)        # RunMode Antilock variable (W.I.P)
+sharedData.append("")           # Encryption Key
+
+#For GPS interface
+sharedData.append("$GPGGA,")    # 7
+sharedData.append(serial.Serial ("/dev/ttyS0"))              #Open port with baud rate
+sharedData.append(0)
+sharedData.append(0)
+sharedData.append(0)
+sharedData.append(0)
+
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = "%.4f" %(position)
+    return position
+
+def getGPSData():
+    received_data = (str)(sharedData[8].readline())
+    GPGGA_data_available = received_data.find(sharedData[7])
+    
+    
+    if (GPGGA_data_available>0):
+        nmea_time = []
+        nmea_latitude = []
+        nmea_longitude = []
+        sharedData[12] = sharedData[9][0]                    #extract time from GPGGA string
+        nmea_latitude = sharedData[9][1]                #extract latitude from GPGGA string
+        nmea_longitude = sharedData[9][3]               #extract longitude from GPGGA string
+        
+        #print("NMEA Time: ", nmea_time,'\n')
+        #print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
+        
+        lat = float(nmea_latitude)                  #convert string into float for calculation
+        longi = float(nmea_longitude)               #convertr string into float for calculation
+        sharedData[10] = convert_to_degrees(lat)
+        sharedData[11] = convert_to_degrees(longi)
+    return [sharedData[10], sharedData[11], sharedData[12][:2] + ":" + sharedData[12][2:4] + ":" + sharedData[12][4:6]]
 
 #For web server
+def encryptData(data,key):
+    return hmac.new(bytes(key , 'utf-8'), msg = bytes(data , 'utf-8'), digestmod = hashlib.sha256).hexdigest()
+
 def getResponseContent(x,startVal,endVal):
     x = x[x.find(startVal) + len(startVal) + 1:x.rfind(endVal)]
     return x
 
-def getGPSData():
-    return [0,0,0] # [GPS Latitute, GPS Longitude, GPS Time]
-
 def setServerValue(val, key):
-    serverResponse = requests.get(url=URL,headers={'Setvalue':val,'Key':key,'Id':sharedData[2]})
+    serverResponse = requests.get(url=URL,headers={'Setvalue':val,'Key':key,'Id':sharedData[2],'Encryptedkey':encryptData(val+key+sharedData[2],sharedData[6])})
     if (serverResponse.ok == False):
         raise Exception("Error when getting server response LOL")
 
@@ -56,10 +94,12 @@ def initialize():
             print("[WEB] Retrying with ID: " + str(sharedData[2]))
         if (sharedData[3].ok):
             if (sharedData[2] == "-1"):
-                newID = json.loads(getResponseContent(sharedData[3].text,"<body>","</body>"))["ID"]
+                JSONData = json.loads(getResponseContent(sharedData[3].text,"<body>","</body>"))
+                newID = JSONData["ID"]
                 print("[WEB] Retreived new ID: " + str(newID))
 
                 sharedData[2] = str(newID)
+                sharedData[6] = str(JSONData["EncryptionKey"])
             GPSData = getGPSData()
 
             print("[WEB] Sending server current status")
@@ -71,17 +111,37 @@ def initialize():
             setServerValue("STP","RunMode")
 
             print("[WEB] Finishing up init")
+            f = open(EncryptionKey_Filepath,'w+')
+            f.write(str(sharedData[6]))
+            f.close()
             f = open(ID_Filepath,'w+')
             f.write(str(newID))
             f.close()
         else:
             print("[WEB] ERROR: Could not get new ID from server. " + str(sharedData[3].status_code) + " status returned!")
     else:
-        f = open(ID_Filepath,"r")
-        sharedData[2] = f.read()
-        f.close()
-        
-        print("[WEB] Starting Unit ID: " + sharedData[2])
+        try:
+            f = open(EncryptionKey_Filepath,"r")
+            sharedData[6] = f.read()
+            f.close()
+            f = open(ID_Filepath,"r")
+            sharedData[2] = f.read()
+            f.close()
+
+            if (len(sharedData[2]) > 0):
+                print("[WEB] Starting Unit ID: " + sharedData[2])
+            else:
+                print("ID file exists but is empty. Recreating file....")
+                if (os.path.exists(EncryptionKey_Filepath)):
+                    os.remove(EncryptionKey_Filepath)
+                if (os.path.exists(ID_Filepath)):
+                    os.remove(ID_Filepath)
+        except:
+            print("Error reading from ID file. Recreating...")
+            if (os.path.exists(EncryptionKey_Filepath)):
+                os.remove(EncryptionKey_Filepath)
+            if (os.path.exists(ID_Filepath)):
+                os.remove(ID_Filepath)
 
     if (sharedData[2] == "-1"):
         raise Exception("[WEB] ERROR: Failed to initialize correctly")
