659062e5cafe987a23835206068f3246029969b8
Techmokid <58498119+Techmokid@users.noreply.github.com>
2022-05-20 01:14:16 +0800

    Added new payment to owing list

--- Active Projects/How Much I Owe People.py
+++ Active Projects/How Much I Owe People.py
@@ -3,5 +3,6 @@ StillOwingToMum = 2320
 StillOwingToMum -= 200 #30/03/2022
 StillOwingToMum -= 200 #13/04/2022
 StillOwingToMum -= 200 #27/04/2022
+StillOwingToMum -= 200 #05/05/2022
 
 print(StillOwingToMum)
