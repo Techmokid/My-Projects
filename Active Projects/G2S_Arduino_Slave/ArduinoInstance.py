15393d105a703849c6fe51a3eda8445fcc449f41
Techmokid <58498119+Techmokid@users.noreply.github.com>
2024-07-24 15:17:30 +0800

    Moved G2S code to seperate git repo

--- Active Projects/G2S_Arduino_Slave/ArduinoInstance.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    import Main
-    exit()
-
-from ArduinoCommunicationsInterface import Arduino
-
-def get_arduino_instance():
-    return Arduino(port="COM4", baudrate=115200)
