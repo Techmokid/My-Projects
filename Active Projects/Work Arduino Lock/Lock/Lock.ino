beef4ee510e246f7456ec9895888068a8ed19567
Techmokid <58498119+Techmokid@users.noreply.github.com>
2024-12-13 19:51:46 +0800

    Edited relay pinmode to mifare project

--- Active Projects/Work Arduino Lock/Lock/Lock.ino
+++ Active Projects/Work Arduino Lock/Lock/Lock.ino
@@ -23,6 +23,7 @@ void setup() {
 
   pinMode(LED_RED_PIN,OUTPUT);
   pinMode(LED_GRN_PIN,OUTPUT);
+  pinMode(RELAY_UNLOCK_PIN,OUTPUT);
   for (int i = 0; i < 3; i++) {
     digitalWrite(LED_RED_PIN,HIGH);
     digitalWrite(LED_GRN_PIN,HIGH);
